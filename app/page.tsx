2820dad7b452c5157e899e12887b869ee479d4d7
GustavsKr <gustavsgo@gmail.com>
2025-11-23 16:52:06 +0200

    updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
 
         {/* Gradient + Navbar wrapper to fade in together */}
-        <div className="absolute top-0 left-0 w-full z-20 opacity-0 animate-fadeIn delay-1000">
+        <div className="absolute top-0 left-0 w-full z-20 opacity-0 animate-fadeIn delay-500">
           {/* Top dark gradient */}
           <div className="absolute top-0 left-0 w-full h-48 bg-linear-to-b from-black/90 to-transparent"></div>
           
