e0bb67cf5360e9b6d8fa0932bde5faf532883605
GustavsKr <gustavsgo@gmail.com>
2025-11-28 13:53:38 +0200

    cleaned up directories

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,15 @@
-import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 import News from "@/components/News";
 import MediaShowcase from "@/components/MediaShowcase";
 import Contact from "@/components/Contact";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full text-white overflow-x-hidden">
-
-      {/* Hero section */}
-<section className="relative w-full h-[117vh] bg-black">
-  {/* Hero image */}
-  <Image
-    src="/hero.jpg"
-    alt="Elzana Sharipova"
-    fill
-    className="object-cover animate-fadeIn"
-    priority
-  />
-
-  {/* Gradient + Navbar wrapper */}
-  <div className="absolute top-0 left-0 w-full z-20 opacity-0 animate-fadeIn delay-500">
-    <div className="absolute top-0 left-0 w-full h-60 bg-linear-to-b from-black/90 to-transparent"></div>
-    <Navbar />
-  </div>
-
-  {/* Diagonal SVG at bottom */}
-<svg
-  className="absolute -bottom-2 w-full h-20 z-10"
-  viewBox="0 0 100 100"
-  preserveAspectRatio="none"
->
-  <polygon points="0,0 100,50 100,100 0,100" fill="#eaeaea" />
-</svg>
-
-
-</section>
-
-{/* Next section background matches SVG */}
-<section className="bg-[#eaeaea]">
-  <News />
-  <MediaShowcase />
-  <Contact />
-</section>
-
+    <main className="w-full overflow-x-hidden">
+      <Hero />
+      <News />
+      <MediaShowcase />
+      <Contact />
     </main>
   );
 }
